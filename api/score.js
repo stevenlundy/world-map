722b83cc791d64599ab72372100745982966299a
Steven Lundy <steven.lundy@gmail.com>
2019-01-09 15:35:30 -0800

    Factor out scoreGuesses to function

--- api/score.js
+++ api/score.js
@@ -1,29 +1,32 @@
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser')
 const jsonParser = bodyParser.json()
 
 const countryMappings = require('./country-decoder')
 const countryCodes = require('./country-codes')
+
+function scoreGuesses(guesses) {
+  let correctAnswers = [];
+  let incorrectAnswers = [];
+  for (let key in guesses) {
+    let countryCode = countryMappings[key];
+    if (!countryCode) continue;
+    let guess = guesses[key].toUpperCase();
+    let possibleAnswers = countryCodes[countryCode].map(s => s.toUpperCase());
+    if (possibleAnswers.includes(guess)) {
+      correctAnswers.push(key);
+    } else {
+      incorrectAnswers.push(key);
+    }
+  }
+  return {correctAnswers, incorrectAnswers};
+}
 
 module.exports = (req, res) => {
   jsonParser(req, res, function(err) {
     if (err) {
       console.log(err)
     } else {
-      console.log(req.body);
-      let correctAnswers = [];
-      let incorrectAnswers = [];
-      let guesses = req.body.guesses;
-      for (let key in guesses) {
-        let countryCode = countryMappings[key];
-        if (!countryCode) return;
-        let guess = guesses[key].toUpperCase();
-        let possibleAnswers = countryCodes[countryCode].map(s => s.toUpperCase());
-        if (possibleAnswers.includes(guess)) {
-          correctAnswers.push(key);
-        } else {
-          incorrectAnswers.push(key);
-        }
-      }
+      let {correctAnswers, incorrectAnswers} = scoreGuesses(req.body.guesses);
       res.end(JSON.stringify({correct: correctAnswers, incorrect: incorrectAnswers}));
     }
   })
